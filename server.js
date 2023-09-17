4680eee989de6fccb7932fea104b82df22168385
sourav11 <143427277+Sourav0211@users.noreply.github.com>
2023-09-16 22:22:27 -0400

    Added server, Axios, and ChartJS

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
 app.use( '/', express.static('public'));
 
-const budget = {
+/*const budget = {
     
     myBudget: [
 
@@ -21,14 +22,50 @@ const budget = {
             budget: 300
         },
     ]
-};
+};*/
+const myBudget= JSON.parse(fs.readFileSync('Budget_Server.json', 'utf8'));
+
+
+/*fetch('http://localhost:3000/Budget_Server.json')
+        .then(res => {
+            return res.json()})
+        
+        .then(data =>{
+            const title = data.map(item => item.title);
+            const budget = data.map(item => item.budget);
+            const ctx = document.getElementById('myPieChart').getContext('2d');
+            
+            new Chart(ctx, {
+                    type: 'pie',
+                    data: {
+                        labels: title,
+                        datasets: [{
+                            data: budget, }]
+                          },     
+                           }
+                   )}
+        ).catch(error => {
+            console.error('error loading jason data:',error)
+        });*/
+                
+
+
+
+
+
+
+
+
+
+
+
 
 app.get('/home',(req,res) => {
     res.send('Hello World');
 });
 
 app.get('/budget' , (req,res) => {
-    res.json(budget);
+    res.json(myBudget);
 });
 
 app.listen(port, () => {
