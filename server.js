cfc0549101172817652d57a3543a34839152fa50
sourav11 <143427277+Sourav0211@users.noreply.github.com>
2023-09-17 20:38:23 -0400

    Added D3JS chart

--- server.js
+++ server.js
@@ -23,49 +23,22 @@ app.use( '/', express.static('public'));
         },
     ]
 };*/
-const myBudget= JSON.parse(fs.readFileSync('Budget_Server.json', 'utf8'));
 
 
-/*fetch('http://localhost:3000/Budget_Server.json')
-        .then(res => {
-            return res.json()})
-        
-        .then(data =>{
-            const title = data.map(item => item.title);
-            const budget = data.map(item => item.budget);
-            const ctx = document.getElementById('myPieChart').getContext('2d');
-            
-            new Chart(ctx, {
-                    type: 'pie',
-                    data: {
-                        labels: title,
-                        datasets: [{
-                            data: budget, }]
-                          },     
-                           }
-                   )}
-        ).catch(error => {
-            console.error('error loading jason data:',error)
-        });*/
+
+
+const myBudget= JSON.parse(fs.readFileSync('Budget_Server.json'));
+const data= JSON.parse(fs.readFileSync('my-data.json'));
                 
-
-
-
-
-
-
-
-
-
-
-
-
 app.get('/home',(req,res) => {
     res.send('Hello World');
 });
 
 app.get('/budget' , (req,res) => {
     res.json(myBudget);
+});
+app.get('/earning' , (req,res) => {
+    res.json(data);
 });
 
 app.listen(port, () => {
